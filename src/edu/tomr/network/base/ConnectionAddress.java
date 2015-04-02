4bf5dce07a716a6ec1a6e33ab13ad8c4626329ce
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-02 14:54:42 -0400

    Initial heart beat module

---
+++ src/edu/tomr/network/base/ConnectionAddress.java
@@ -0,0 +1,41 @@
+package edu.tomr.network.base;
+
+public final class ConnectionAddress {
+
+	private String ipAddress;
+	private int portNumber;
+	
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	public int getPortNumber() {
+		return portNumber;
+	}
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	
+	public ConnectionAddress() {}
+	
+	public ConnectionAddress(String ipAddress, int portNumber) {
+		this.ipAddress = ipAddress;
+		this.portNumber = portNumber;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		ConnectionAddress address = (ConnectionAddress)obj;
+		if(this.ipAddress.equalsIgnoreCase(address.getIpAddress()) &&
+				this.portNumber == address.getPortNumber())
+			return true;
+		
+		return false;
+	}
+	
+	
+	
+}
---
+++ src/edu/tomr/protocol/HeartBeatMessage.java
@@ -0,0 +1,39 @@
+package edu.tomr.protocol;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import edu.tomr.network.base.ConnectionAddress;
+
+public class HeartBeatMessage extends Message {
+
+	private ConnectionAddress sender;
+	private Date timeStamp;
+	
+	public ConnectionAddress getSender() {
+		return sender;
+	}
+	public void setSender(ConnectionAddress sender) {
+		this.sender = sender;
+	}
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	public HeartBeatMessage(ConnectionAddress sender) {
+		
+		this.sender = sender;
+		this.timeStamp = Calendar.getInstance().getTime();
+	}
+	
+	@Override
+	public String toString() {
+		
+		return new StringBuilder().append(sender.getIpAddress()).append(sender.getPortNumber()).toString();
+	}
+	
+	
+}
