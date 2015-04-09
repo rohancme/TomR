a94ed2337693945fe42cb65cc828e90d4b0cf17f
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-09 17:33:01 -0400

    Node DB module: Exposing methods to network module

--- src/edu/tomr/node/base/Node.java
+++ src/edu/tomr/node/base/Node.java
@@ -3,8 +3,11 @@ package edu.tomr.node.base;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.tomr.hash.IConsistentHashing;
 import edu.tomr.node.map.operations.IMapOperation;
 import edu.tomr.node.map.operations.MapOperation;
+import edu.tomr.protocol.ClientMessage;
+import edu.tomr.protocol.ClientRequestPayload;
 
 /*
  * Should containing a network module to handle the connections
@@ -14,10 +17,77 @@ public class Node {
 	
 	private Map<String, byte[]> inMemMap;
 	private IMapOperation operation;
+	private static IConsistentHashing hasher;
+	private final String selfIpAddress;
 	
-	public Node(){
+	public String getSelfAddress() {
+		return this.selfIpAddress;
+	}
+	
+	public Node(String selfIpAdd){
+		
+		this.selfIpAddress = selfIpAdd;
 		inMemMap = new HashMap<String, byte[]>();
 		operation = new MapOperation(inMemMap);
+		if(hasher == null){
+			//instantiate hasher
+			//hasher =  new ConsistentHashing();
+		}
+	}
+	
+	/**
+	 * This method handles the client request received by the load balancer
+	 * 
+	 * @param	message			message containing the KV pair from the load balancer
+	 * @return	isToForward  	whether the message has to be forwarded to the
+	 * 							destination node
+	 */
+	public boolean handleClientRequest(ClientMessage message) {
+		
+		boolean isToForward;
+		String ipAddress = hasher.calculateNode(message.getPayload().getKey());
+		message.setDestinationNode(ipAddress);
+		
+		if(ipAddress.equalsIgnoreCase(getSelfAddress())) {
+			
+			new Thread(new Runnable() {
+			    public void run() {
+			    	handleNodeRequest(message);
+			    }
+			}).start();
+			isToForward = false;
+		} else {
+			message.setDestinationNode(ipAddress);
+			isToForward = true;
+		}
+		
+		return isToForward;
+	}
+	
+	/**
+	 * This method handles the requests meant for this node
+	 * 
+	 * @param message	message containing the KV pair from the load balancer	
+	 * @return
+	 */
+	public ClientRequestPayload handleNodeRequest(ClientMessage message) {
+		
+		ClientRequestPayload tempPayload = message.getPayload();
+		
+		switch(message.getRequestType()) {
+			case ADD: 	operation.put(tempPayload.getKey(), tempPayload.getValue());
+						break;
+						
+			case GET:	tempPayload.setValue(operation.get(tempPayload.getKey()));
+						break;
+						
+			case DELETE: operation.delete(tempPayload.getKey());
+						 break;
+		
+			case UPDATE: break;
+			
+		}
+		return tempPayload;
 	}
 	
 }
