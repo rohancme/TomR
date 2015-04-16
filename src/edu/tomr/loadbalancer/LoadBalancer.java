5c6a61755837f627d2bb6d7f202ae5e00cad3d79
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-16 19:01:59 -0400

    Started servicing ports for adding message

--- src/edu/tomr/loadbalancer/LoadBalancer.java
+++ src/edu/tomr/loadbalancer/LoadBalancer.java
@@ -9,9 +9,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import network.Connection;
+import network.NetworkConstants;
 import network.NetworkException;
 import network.NetworkUtilities;
 import network.requests.NWRequest;
+import edu.tomr.handler.AddMessageHandler;
+import edu.tomr.handler.PortRequestHandler;
 import edu.tomr.network.heartbeat.server.ServerBeatController;
 import edu.tomr.protocol.StartupMessage;
 import edu.tomr.utils.ConfigParams;
@@ -21,94 +24,104 @@ public class LoadBalancer {
 	static {
 		 ConfigParams.loadProperties();
 	}
-	
+
 	static int startupMsgPort=5000;
 	static int clientPort=6000;
 	static int sizeOfThreadPool=20;
 	private ServerBeatController beatController;
-	
+
 	public LoadBalancer() {
 		beatController = new ServerBeatController();
 	}
-	
+
 	public void startHeartBeatServer() {
 		beatController.start();
 	}
-	
+
 	public void startServer() {
-	
+
 		NetworkUtilities utils = null;
-		
+
 		try {
 			utils = new NetworkUtilities();
 		} catch (NetworkException e) {
 			e.printStackTrace();
 		}
-		
+
 		List<String> nodeAddresses = ConfigParams.getIpAddresses();
-		
+
 		//make the last one connectFirst
 		int i;
 		for(i=0;i<nodeAddresses.size()-1;i++){
-			
+
 			List<String> neighbors=generateNeighborList(i,nodeAddresses);
 			StartupMessage msg=new StartupMessage("Testing123", neighbors, nodeAddresses);
 			NWRequest startupRequest=utils.getNewStartupRequest(msg);
-			
+
 			Connection temp_connection=new Connection(nodeAddresses.get(i),startupMsgPort);
-			
+
 			temp_connection.send_request(startupRequest);
-	
+
 		}
-		
+
 		List<String> neighbors = generateNeighborList(i,nodeAddresses);
 		StartupMessage msg = new StartupMessage("Testing123", neighbors, nodeAddresses, true);
 		NWRequest startupRequest = utils.getNewStartupRequest(msg);
-		
+
 		Connection temp_connection=new Connection(nodeAddresses.get(i),startupMsgPort);
-		
+
 		temp_connection.send_request(startupRequest);
 	}
 
 	private static List<String> generateNeighborList(int i, List<String> iPAddresses) {
-		
+
 		List<String> neighbors=new ArrayList<String>();
-		
+
 		if(i+1>iPAddresses.size()-1){
 			neighbors.add(iPAddresses.get(0));
 		}
 		else{
 			neighbors.add(iPAddresses.get(i));
 		}
-					
+
 		return neighbors;
 	}
-	
+
 	private void listenForClients(int clientPort) {
 		try {
 			ServerSocket clientSocket = new ServerSocket(clientPort);
 			ExecutorService threadPool = Executors.newFixedThreadPool(sizeOfThreadPool);
-			
+
 			//Listen for connections
 			while(true){
 				Socket clientConnection = clientSocket.accept();
 				threadPool.submit(new clientConnectionHandler(clientConnection));
-				
+
 			}
 		} catch (IOException e) {
 			System.out.println("Error while trying to connect with the client");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	private void startServicingPorts() {
+
+		//Start servicing add message port LB_ADD_LISTEN_PORT
+		PortRequestHandler<AddMessageHandler> addMsgHandler = new PortRequestHandler<AddMessageHandler>(NetworkConstants.LB_ADD_LISTEN_PORT,
+				"edu.tomr.handler.AddMessageHandler");
+		Thread addMsgThread = new Thread(addMsgHandler);
+		addMsgThread.start();
+	}
+
 	public static void main(String[] args) {
-		
+
 		LoadBalancer loadBalancer = new LoadBalancer();
 		//loadBalancer.startHeartBeatServer();
 		loadBalancer.startServer();
 		loadBalancer.listenForClients(clientPort);
+		loadBalancer.startServicingPorts();
 	}
 
-	
+
 }
