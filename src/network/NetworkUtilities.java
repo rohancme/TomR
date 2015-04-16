20c1144d3cdb12166bb8220b8d707630667b2c61
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-16 18:54:42 -0400

    Included DBMessage and BreakFormationMessage support in network

--- src/network/NetworkUtilities.java
+++ src/network/NetworkUtilities.java
@@ -8,13 +8,15 @@ import java.util.Enumeration;
 import java.util.UUID;
 
 import network.requests.NWRequest;
+import edu.tomr.protocol.BreakFormationMessage;
+import edu.tomr.protocol.DBMessage;
 import edu.tomr.protocol.NeighborMessage;
 import edu.tomr.protocol.StartupMessage;
 
 public class NetworkUtilities {
-	
+
 	private final String IP;
-	
+
 	public NetworkUtilities() throws NetworkException{
 		//following code is from:http://stackoverflow.com/a/18945245
 		String ipAddress = null;
@@ -36,7 +38,7 @@ public class NetworkUtilities {
 	                }
 	            }
 	        }
-	    }	    
+	    }
 	    if(ipAddress==null){
 	    	throw new NetworkException("Error initializing IP address. Try defining the IP address in the config file.");
 	    }
@@ -44,43 +46,48 @@ public class NetworkUtilities {
 	    	this.IP=ipAddress;
 	    }
 	}
-	
+
 	public NetworkUtilities(String IP){
 		this.IP=IP;
 	}
-	
+
 	public String getSelfIP(){
 		return this.IP;
 	}
-	
+
 	public String generate_req_id(){
 		return (IP+UUID.randomUUID());
 	}
-	
-	
+
+
 	/*public NetworkPacket<NewClientConnectionRequest> getNewClientConnectionRequest(Message msg){
 		NewClientConnectionRequest request=new NewClientConnectionRequest(this.generate_req_id(),msg);
 		return new NetworkPacket<NewClientConnectionRequest>(request);
 	}*/
-	
+
 	public NWRequest getNewNeighborConnectionRequest(NeighborMessage msg){
 		NWRequest request=new NWRequest(this.generate_req_id(),msg);
 		return request;
 	}
-	
-	/*public NWRequest getNewCloseRequest(Message msg){	
+
+	/*public NWRequest getNewCloseRequest(Message msg){
 		CloseRequest request=new CloseRequest(this.generate_req_id(),msg);
 		return new NetworkPacket<CloseRequest>(request);
 	}*/
-		
-	public NWRequest getNewStartupRequest(StartupMessage msg){	
+
+	public NWRequest getNewStartupRequest(StartupMessage msg){
 		NWRequest request=new NWRequest(this.generate_req_id(),msg);
 		return request;
 	}
 	
-	/*public NetworkPacket<DBRequest> getNewDBRequest(Message msg){	
-		DBRequest request=new DBRequest(this.generate_req_id(),msg);
-		return new NetworkPacket<DBRequest>(request);
-	}*/
+	public NWRequest getNewDBRequest(DBMessage msg,String destIP){	
+		NWRequest request=new NWRequest(this.generate_req_id(),msg,this.getSelfIP(),destIP);
+		return request;
+	}
+
+	public NWRequest getNewBreakFormRequest(BreakFormationMessage msg)  {
+		NWRequest request = new NWRequest(this.generate_req_id(), msg, this.getSelfIP());
+		return request;
+	}
 
 }
--- src/network/requests/NWRequest.java
+++ src/network/requests/NWRequest.java
@@ -6,47 +6,64 @@ import java.util.UUID;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import edu.tomr.protocol.BreakFormationMessage;
 import edu.tomr.protocol.ClientServiceMessage;
 import edu.tomr.protocol.NeighborMessage;
 import edu.tomr.protocol.NewNeighborConnectionMessage;
 import edu.tomr.protocol.StartupMessage;
+import edu.tomr.protocol.DBMessage;
 import network.NetworkConstants;
 
 public class NWRequest {
-	
+
 	@JsonProperty protected final String request_id;
 	@JsonProperty protected final String request_type;
+	@JsonProperty protected String destIP=null;
+	@JsonProperty protected String srcIP=null;
 	
+	
+
 	@JsonProperty protected StartupMessage startupMessage=null;
 	@JsonProperty protected NewNeighborConnectionMessage newNeighborConnectionMessage=null;
 	@JsonProperty protected NeighborMessage neighborMessage=null;
+	@JsonProperty protected DBMessage dBMessage=null;
+	@JsonProperty protected BreakFormationMessage breakFromMessage=null;
 	@JsonProperty protected ClientServiceMessage serviceMessage=null;
-	
+
 	//only for JACKSON
+	@SuppressWarnings("unused")
 	private NWRequest(){
 		this.request_type="UNKNOWN";
 		this.request_id="NA";
 	}
-	
-	
+
+	//a startup message has no use for source or destiantion IP
 	public NWRequest(String req_id,StartupMessage msg){
 		this.request_id=req_id;
 		this.startupMessage=msg;
 		this.request_type=NetworkConstants.requestToString(Requests.STARTUP);
 	}
-	
+
 	public NWRequest(String req_id,NewNeighborConnectionMessage msg){
 		this.request_id=req_id;
 		this.newNeighborConnectionMessage=msg;
 		this.request_type=NetworkConstants.requestToString(Requests.NEW_NEIGHBOR_CONNECTION);
 	}
-	
+
 	public NWRequest(String req_id,NeighborMessage msg){
 		this.request_id=req_id;
 		this.neighborMessage=msg;
 		this.request_type=NetworkConstants.requestToString(Requests.NEIGHBOR_MESSAGE);
 	}
-	
+
+	public NWRequest(String req_id,DBMessage msg,String SourceIP,String DestinationIP){
+		this.request_id=req_id;
+		this.dBMessage=msg;
+		this.request_type=NetworkConstants.requestToString(Requests.DB_OPERATION);
+		this.srcIP=SourceIP;
+		this.destIP=DestinationIP;
+	}
+
 	public NWRequest(String req_id, ClientServiceMessage msg) {
 		this.request_id=req_id;
 		this.serviceMessage=msg;
@@ -54,17 +71,23 @@ public class NWRequest {
 		
 	}
 
+	public NWRequest(String req_id, BreakFormationMessage msg, String SourceIP){
+		this.request_id=req_id;
+		this.breakFromMessage=msg;
+		this.request_type=NetworkConstants.requestToString(Requests.BREAK_FORM);
+		this.srcIP=SourceIP;
+	}
 
 	@JsonProperty("request_type")
 	public String getRequestType(){
 		return this.request_type;
 	}
-	
+
 	@JsonProperty("request_id")
 	public String getRequestID(){
 		return this.request_id;
 	}
-	
+
 	@JsonProperty("startupMessage")
 	public StartupMessage getStartupMessage() {
 		return this.startupMessage;
@@ -79,6 +102,16 @@ public class NWRequest {
 	public NeighborMessage getNeighborMessage() {
 		return this.neighborMessage;
 	}
-	
-	
+
+	@JsonProperty("destIP")
+	public String getDestinationIP() {
+		return destIP;
+	}
+
+	@JsonProperty("srcIP")
+	public String getSourceIP() {
+		return srcIP;
+	}
+
+
 }
