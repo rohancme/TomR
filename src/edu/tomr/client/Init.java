6f2f10a0d1800f2c882b0f47e22c7664d1ef50e6
VINAY <frags.vinay@gmail.com>
2015-04-16 00:23:53 -0400

    Load Balancer Made the initial changes required in load balancer package

---
+++ src/edu/tomr/client/Init.java
@@ -0,0 +1,18 @@
+package edu.tomr.client;
+
+public class Init {
+	
+private static void getKeyValue() {
+		
+		
+	}
+
+
+	
+	public static void main(String[] args) {
+		getKeyValue();
+
+	}
+
+	
+}
