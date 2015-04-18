4980d6074ef10e13320847c967e66454f8e18c82
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-18 19:18:05 -0400

    Added method signatures for consistent hashing

--- src/edu/tomr/hash/ConsistentHashing.java
+++ src/edu/tomr/hash/ConsistentHashing.java
@@ -1,6 +1,7 @@
 package edu.tomr.hash;
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
@@ -8,6 +9,7 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
+import edu.tomr.client.KeyValuePair;
 import edu.tomr.utils.ConfigParams;
 
 
@@ -18,12 +20,12 @@ public class ConsistentHashing {
 	public static void calculateCircle(List<String> nodes){
 		// Evenly distributing the nodes on the unit circle using the concept of virtual replicas
 		try{
-		for(String node : nodes){
-			unitCircle.put(getHash(node), node);
-			unitCircle.put((getHash(node) + .33)%.99, node);
-			//unitCircle.put((getHash(node) + .6)%.99, node);
+			for(String node : nodes){
+				unitCircle.put(getHash(node), node);
+				unitCircle.put((getHash(node) + .33)%.99, node);
+				//unitCircle.put((getHash(node) + .6)%.99, node);
+			}
 		}
-	}
 		catch(NullPointerException e){
 			System.out.println("The nodes have been hashed in a wrong manner");
 			e.printStackTrace();
@@ -49,13 +51,13 @@ public class ConsistentHashing {
 		}
 
 		try{
-		// Find the position of the key on the unit circle representation.
-		Double hashOfKey = getKeyHash(key);
-		// Get the nearest node in the Anti Clock-wise direction on the unit circle
-		Double nodeNumber = getFirstNode(hashOfKey, unitCircle);
-		//Get the IP address of the node nearest to the key
-		String nodeName = unitCircle.get(nodeNumber);
-		return nodeName;
+			// Find the position of the key on the unit circle representation.
+			Double hashOfKey = getKeyHash(key);
+			// Get the nearest node in the Anti Clock-wise direction on the unit circle
+			Double nodeNumber = getFirstNode(hashOfKey, unitCircle);
+			//Get the IP address of the node nearest to the key
+			String nodeName = unitCircle.get(nodeNumber);
+			return nodeName;
 		}
 		catch(NullPointerException e){
 			System.out.println("Error while trying to find the node to send the string to");
@@ -68,60 +70,78 @@ public class ConsistentHashing {
 	}
 
 	// Method to get the nearest node to the key
-		static Double getFirstNode(Double key, TreeMap<Double, String> circle ){
-			Double firstKey = 0.0;
-			for(Entry<Double, String> nodeHash : circle.entrySet()) {
-				Double nodeValue = nodeHash.getKey();
-				if(nodeValue < key){
-					if(nodeValue > firstKey){
-						firstKey = nodeValue;
-					}
-
+	static Double getFirstNode(Double key, TreeMap<Double, String> circle ){
+		Double firstKey = 0.0;
+		for(Entry<Double, String> nodeHash : circle.entrySet()) {
+			Double nodeValue = nodeHash.getKey();
+			if(nodeValue < key){
+				if(nodeValue > firstKey){
+					firstKey = nodeValue;
 				}
-			}
-			// If there is no node before this key, then this key is assigned to the last node
-			if(firstKey == 0.0){
-				firstKey = circle.lastKey();
-			}
 
-			return firstKey;
-
+			}
 		}
-		// Method that is used to hash the nodes onto the unit circle
-		static double getHash(String newString){
-			double hashofString = 0;
-			try{
-				// The murmur hash is an effective way to uniformly hash different string
-				HashFunction hf = Hashing.murmur3_128();
-				HashCode hc = hf.newHasher().putString(newString).hash();
-				byte[] byteArray = hc.asBytes();
-				ByteBuffer buffer = ByteBuffer.wrap(byteArray);
-				hashofString = buffer.getShort();
-
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-			return Math.abs(hashofString)%.99;
-
-		}
-		// Method that is used to hash the keys onto the unit circle
-		static double getKeyHash(String newString){
-			double hashofString = 0;
-			try{
-				// The murmur hash is an effective way to uniformly hash different string
-				HashFunction hf = Hashing.murmur3_128();
-				HashCode hc = hf.newHasher().putString(newString).hash();
-				byte[] byteArray = hc.asBytes();
-				ByteBuffer buffer = ByteBuffer.wrap(byteArray);
-				hashofString = buffer.getShort();
-
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-			return Math.abs(hashofString)%.99;
-
+		// If there is no node before this key, then this key is assigned to the last node
+		if(firstKey == 0.0){
+			firstKey = circle.lastKey();
 		}
 
+		return firstKey;
+
+	}
+	// Method that is used to hash the nodes onto the unit circle
+	static double getHash(String newString){
+		double hashofString = 0;
+		try{
+			// The murmur hash is an effective way to uniformly hash different string
+			HashFunction hf = Hashing.murmur3_128();
+			HashCode hc = hf.newHasher().putString(newString).hash();
+			byte[] byteArray = hc.asBytes();
+			ByteBuffer buffer = ByteBuffer.wrap(byteArray);
+			hashofString = buffer.getShort();
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return Math.abs(hashofString)%.99;
+
+	}
+	// Method that is used to hash the keys onto the unit circle
+	static double getKeyHash(String newString){
+		double hashofString = 0;
+		try{
+			// The murmur hash is an effective way to uniformly hash different string
+			HashFunction hf = Hashing.murmur3_128();
+			HashCode hc = hf.newHasher().putString(newString).hash();
+			byte[] byteArray = hc.asBytes();
+			ByteBuffer buffer = ByteBuffer.wrap(byteArray);
+			hashofString = buffer.getShort();
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return Math.abs(hashofString)%.99;
+
+	}
+
+	/**
+	 * Returns the keys to be redistributed to other nodes
+	 * @param keys
+	 * @return
+	 */
+	public static Map<String, List<String>> redistributeKeys(List<String> keys) {
+
+
+		return null;
+	}
+
+	/**
+	 * Method to update the unit circle after addition or removal of nodes
+	 * @param ipAddresses
+	 */
+	public static void updateCircle(List<String> ipAddresses) {
+
+	}
 }
