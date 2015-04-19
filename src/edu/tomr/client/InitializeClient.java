c7d710993ce25c1656f43bb988785cc547c274db
muchhalsagar88 <muchhalsagar88@gmail.com>
2015-04-19 13:43:45 -0400

    Test code from client

--- src/edu/tomr/client/InitializeClient.java
+++ src/edu/tomr/client/InitializeClient.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 import network.Connection;
 import network.NWResponse;
+import network.NetworkException;
+import network.NetworkUtilities;
 import network.requests.NWRequest;
+import edu.tomr.protocol.ClientInfo;
+import edu.tomr.protocol.ClientRequestPayload;
+import edu.tomr.protocol.ClientRequestType;
 import edu.tomr.protocol.ClientServiceMessage;
 import edu.tomr.protocol.DBMessage;
 import edu.tomr.utils.ConfigParams;
 
 public class InitializeClient {
-	
-	
-	
+
+
+
 	static{
 		//Get the load balancer IP address
 		ConfigParams.loadProperties();
 	}
-	
+
 	public static String serverIP;
 	private static int lbPort=6000;
 	private static int servicerNodePort=5003;
-	
+
 private static KeyValuePair getKeyValue() {
 		System.out.println("Enter the key :");
 		Scanner input = new Scanner(System.in);
@@ -29,18 +34,18 @@ private static KeyValuePair getKeyValue() {
 		System.out.println("Enter the Value for this key :");
 		String value = input.nextLine();
 		return (new KeyValuePair(key, value));
-		
+
 	}
 
 	private static ClientServiceMessage getServiceMessage() {
 		//serverIP = ConfigParams.getProperty(LB_IP);
 		serverIP = "";
-		
+
 		//Connect to Load balancer and get servicerIP
 		Connection lbConnection=new Connection(serverIP,lbPort);
 		NWResponse response=lbConnection.getnextResponse();
-		
-		
+
+
 		return response.getClientServiceMsg();
 	}
 
@@ -48,21 +53,32 @@ private static KeyValuePair getKeyValue() {
 
 	public static void main(String[] args) {
 		KeyValuePair inputTuple=null;
-		inputTuple = getKeyValue();
+
+		NetworkUtilities utils = null;
+		try {
+			utils = new NetworkUtilities();
+		} catch (NetworkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//inputTuple = getKeyValue();
 		ClientServiceMessage serviceMessage = getServiceMessage();
 		//Send the request to the particular node.
 		Connection nodeConnection=new Connection(serviceMessage.getServiceIPAddress(),servicerNodePort);
 		//this is dummy code. will need to be updated once we figure out how we're accepting client queries
-		DBMessage query=new DBMessage();
-		NWRequest request=new NWRequest(serviceMessage.getPayloadID(),query);
-		nodeConnection.send_request(request);
-		//this is block wait method
-		NWResponse response=nodeConnection.getnextResponse();
-		
-		System.out.println(response.getAckMsg().toString());
+		for(int i=1; i<=3; i++){
+			DBMessage query=new DBMessage(ClientRequestType.ADD, new ClientRequestPayload("File-"+i, new String("File-"+i).getBytes()), new ClientInfo(utils.getSelfIP()), serviceMessage.getPayloadID());
+			NWRequest request=new NWRequest(serviceMessage.getPayloadID(),query);
+			nodeConnection.send_request(request);
+			//this is block wait method
+			NWResponse response=nodeConnection.getnextResponse();
+
+			System.out.println(response.getAckMsg().toString());
+		}
 	}
 
 
 
-	
+
 }
